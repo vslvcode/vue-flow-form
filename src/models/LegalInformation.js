f85b0be7b067f6362e991377c69e39f4576d734f
vslvcode <vslvcode@yandex.ru>
2021-04-23 23:23:19 +0300

    Изменение футера

--- src/models/LegalInformation.js
+++ src/models/LegalInformation.js
@@ -1,9 +1,21 @@
 export default class LegalInformation {
   constructor(options) {
-    this.show = false
     this.disclaimer = ''
-    this.privacyHTML = ''
+    // example:
+    //this.privacy = {
+    //  text: 'Privacy policy',
+    //  href: '/privacy'
+    //}
+    this.agreement = null
+    this.privacy = null
 
     Object.assign(this, options || {})
+  }
+
+  getHTML(elem) {
+    return '<a class="link" ' +
+      'target="_blank" rel="noopener noreferrer" href="' +
+      elem.href + '" ' + 'style="color: cornflowerblue !important;">' +
+      '' + elem.text + '</a>'
   }
 }
